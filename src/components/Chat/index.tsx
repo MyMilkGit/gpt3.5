cc34374f9610a28d360fbea71c29329c97985bb9
lilh <1963119666@qq.com>
2023-05-06 15:33:03 +0800

    202305061532

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -176,6 +176,7 @@ export default function () {
     })
     console.info(JSON.stringify({messages}));
     console.log(JSON.stringify({messages}));
+    console.error(JSON.stringify({messages}));
     if (!response.ok) {
       const res = await response.json()
       throw new Error(res.error.message)
